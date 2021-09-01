7b7eff0309aa8fbce1fba1ca82236bcd9cc6e35a
SergeevSezar <sergeev.sezar@yandex.ru>
2021-09-01 23:22:17 +0900

    add adapter recycler view

--- app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.adapter.CoinInfoAdapter
+import com.example.cryptoapp.pojo.CoinPriceInfo
 
 class CoinPriceListActivity : AppCompatActivity() {
 
@@ -12,12 +15,20 @@ class CoinPriceListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
+
+        val rvCoinPriceList = findViewById<RecyclerView>(R.id.rvCoinPriceList)
+
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onClick(coinPriceInfo: CoinPriceInfo) {
+                Log.d("ON CLICK TEST", coinPriceInfo.fromSymbol)
+            }
+        }
+        rvCoinPriceList.adapter = adapter
+
         viewModel = ViewModelProvider.AndroidViewModelFactory(application).create(CoinViewModel::class.java)
         viewModel.priceList.observe(this, {
-            Log.d("TEST_OF_LOADING", "Success in Activity: $it")
-        })
-        viewModel.getDetailInfo("BTC").observe(this, {
-            Log.d("TEST_OF_LOADING", "Success in Activity: $it")
+            adapter.coinInfoList = it
         })
     }
 }
---
+++ app/src/main/java/com/example/cryptoapp/adapter/CoinInfoAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.cryptoapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.R
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import com.squareup.picasso.Picasso
+
+class CoinInfoAdapter(private val context: Context) : RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+
+    var coinInfoList: List<CoinPriceInfo> = listOf()
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
+
+    var onCoinClickListener: OnCoinClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
+       val view = LayoutInflater.from(parent.context).inflate(R.layout.item_coin_info, parent, false)
+        return CoinInfoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
+        val coin = coinInfoList[position]
+        with(holder) {
+            with(coin) {
+
+                val symbolsTemplate = context.resources.getString(R.string.symbols_template)
+                val lastUpdTemplate = context.resources.getString(R.string.symbols_last_upd)
+
+                tvSymbols.text = String.format(symbolsTemplate, fromSymbol, toSymbol)
+                tvPrice.text = price.toString()
+                tvLastUpd.text = String.format(lastUpdTemplate, getFormattedTime())
+                Picasso.get().load(getFullImageUrl()).into(ivLogoCoin)
+            }
+        }
+        holder.itemView.setOnClickListener {
+            onCoinClickListener?.onClick(coin)
+        }
+    }
+
+    override fun getItemCount() = coinInfoList.size
+
+    inner class CoinInfoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvSymbols = itemView.findViewById<TextView>(R.id.tvSymbols)
+        val tvPrice = itemView.findViewById<TextView>(R.id.tvPrice)
+        val tvLastUpd = itemView.findViewById<TextView>(R.id.tvTimeLastUpd)
+        val ivLogoCoin = itemView.findViewById<ImageView>(R.id.ivLogoCoin)
+    }
+
+    interface OnCoinClickListener {
+        fun onClick(coinPriceInfo: CoinPriceInfo)
+    }
+}
