91590fef0ae3bd1434e3343d91b6dd3137c72319
SergeevSezar <sergeev.sezar@yandex.ru>
2021-12-17 06:37:31 +0900

    added change database

--- app/src/main/java/com/example/cryptoapp/data/network/ApiService.kt
+++ app/src/main/java/com/example/cryptoapp/data/network/ApiService.kt
@@ -1,7 +1,7 @@
 package com.example.cryptoapp.data.network
 
-import com.example.cryptoapp.data.model.CoinInfoListOfData
-import com.example.cryptoapp.data.model.CoinPriceInfoRawData
+import com.example.cryptoapp.data.network.model.CoinInfoJsonContainerDto
+import com.example.cryptoapp.data.network.model.CoinNamesListDto
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -13,14 +13,14 @@ interface ApiService {
         @Query(QUERY_PARAM_API_KEY) apiKey: String = "22b03245f84b308d7b4e7b4c25404e923b67f9829e19be92ab24d252296320cf",
         @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
         @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
-    ): Single<CoinInfoListOfData>
+    ): Single<CoinNamesListDto>
 
     @GET("pricemultifull")
     fun getFullPriceList(
         @Query(QUERY_PARAM_API_KEY) apiKey: String = "22b03245f84b308d7b4e7b4c25404e923b67f9829e19be92ab24d252296320cf",
         @Query(QUERY_PARAM_FROM_SYMBOL) fSyms: String,
         @Query(QUERY_PARAM_TO_SYMBOLS) tsyms: String = CURRENCY
-    ): Single<CoinPriceInfoRawData>
+    ): Single<CoinInfoJsonContainerDto>
 
     companion object{
         private const val QUERY_PARAM_API_KEY = "api_key"
