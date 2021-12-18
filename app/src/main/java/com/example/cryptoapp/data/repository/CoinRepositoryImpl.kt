543c2690503eee43590ced26843df4e3a6c44b79
SergeevSezar <sergeev.sezar@yandex.ru>
2021-12-19 02:26:00 +0900

    added worker for update foreground data base

--- app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -3,12 +3,14 @@ package com.example.cryptoapp.data.repository
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import com.example.cryptoapp.data.database.AppDatabase
 import com.example.cryptoapp.data.mapper.CoinMapper
 import com.example.cryptoapp.data.network.ApiFactory
 import com.example.cryptoapp.domain.CoinInfo
 import com.example.cryptoapp.domain.CoinRepository
-import kotlinx.coroutines.delay
+import com.example.cryptoapp.workers.RefreshDataWorker
 
 class CoinRepositoryImpl(
     private val application: Application
@@ -32,18 +34,12 @@ class CoinRepositoryImpl(
         }
     }
 
-    override suspend fun loadData() {
-        while (true) {
-            try {
-                val topCoins = apiService.getTopCoinsInfo(limit = 50)
-                val fromSymbols = mapper.mapNamesListToString(topCoins)
-                val jsonContainer = apiService.getFullPriceList(fSyms = fromSymbols)
-                val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
-                val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
-                coinInfoDao.insertPriceList(dbModelList)
-            } catch (e: Exception) {
-            }
-            delay(10000)
-        }
+    override fun loadData() {
+        val workManager = WorkManager.getInstance(application)
+        workManager.enqueueUniqueWork(
+            RefreshDataWorker.NAME_WORKER,
+            ExistingWorkPolicy.REPLACE,
+            RefreshDataWorker.makeRequest()
+        )
     }
 }
